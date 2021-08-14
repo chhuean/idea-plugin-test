90d17bab3e517740cda6d654d1296420aca4c030
GitHub Action <action@github.com>
2021-08-14 07:38:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/chhuean/ideaplugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chhuean/ideaplugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chhuean.ideaplugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chhuean.ideaplugintest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/chhuean/ideaplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chhuean/ideaplugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chhuean.ideaplugintest.services
 
+import com.github.chhuean.ideaplugintest.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
